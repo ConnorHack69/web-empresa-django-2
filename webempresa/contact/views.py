59a1c59d2bda1eabd0a2fce5d34220ba794d76b3
Iván Córcoles <cgehminecraft@gmail.com>
2018-09-08 15:38:44 +0200

    Proyecto añadido

---
+++ webempresa/contact/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.core.mail import EmailMessage
+from .form import ContactForm
+
+# Create your views here.
+def contact(request):
+    contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = request.POST.get('name','')
+            email = request.POST.get('email','')
+            content = request.POST.get('content','')
+            # Suponemos que todo ha ido bien y redireccionamos
+            email = EmailMessage(
+                "Nuevo mensaje de contacto",
+                "De {} <{}>\n\nEscribió:\n\n{}".format(name, email, content),
+                "no-contestar@miweb.com",
+                ["ivan-f22072@inbox.mailtrap.io"],
+                reply_to=[email]
+            )
+            try:
+                email.send()
+                return redirect(reverse('contact')+"?ok")
+            except:
+                return redirect(reverse('contact')+"?fail")
+
+    return render(request, "contact/contact.html", {'form':contact_form})
